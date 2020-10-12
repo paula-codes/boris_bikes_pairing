2427cad1b9f54f16a4558e1aa53e857207efa770
Paula Gavenyte <paula.gavenyte@gmail.com>
2020-10-12 16:16:42 +0100

    challenge9 swap

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,3 +1,9 @@
 class DockingStation
 
+  def release_bike
+    return true
+  end
+
 end
+
+# docking_station = DockingStation.new
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,14 @@
 require "docking_station"
 
-describe DockingStation do
+  describe DockingStation do
 
-end
+    describe "#release_bike" do
+      it "return true to release the bike" do
+      
+        actual_value = subject.release_bike
+        expected_value = true
+        expect(actual_value).to eq expected_value
+      end
+
+    end
+  end
